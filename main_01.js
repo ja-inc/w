3e744a696772cd0a13d31fc09aa4d7727e9fe3c8
Jheb Krueger <jheb93@gmail.com>
2015-09-02 03:31:30 +0700

    Create main_01.js

---
+++ main_01.js
@@ -0,0 +1,8 @@
+// media-nav menu
+$(document).ready(function () {
+    selectnav( 'menu-main', {
+        label: 'Menu here ', nested: true,
+        autoselect: false,
+        indent: '-'
+    });
+});
