a4a5afb1d642d639bdd71c7dc8e37db8986a6da9
Jheb Krueger <jheb93@gmail.com>
2015-09-02 03:46:33 +0700

    Update main_01.js

--- main_01.js
+++ main_01.js
@@ -1,4 +1,6 @@
-// media-nav menu
+//---------------------------------
+//----- media navbar menu
+//---------------------------------
 $(document).ready(function () {
     selectnav( 'menu-main', {
         label: 'Menu here ', nested: true,
@@ -6,7 +8,9 @@ $(document).ready(function () {
         indent: '-'
     });
 });
-//menu page highlight
+//---------------------------------
+//----- menu page highlight
+//---------------------------------
 $(function () {
     var url = window.location.href;
     $('#menu-wrapper a').each(function () {
@@ -16,3 +20,28 @@ $(function () {
         }
     });
 });
+//---------------------------------
+//----- auto tag url (text-to-url)
+//---------------------------------
+$(document).ready(function () {
+    var p = $("div .post-body div").contents().filter(function () {
+        return this.nodeType === 3
+    }).replaceWith(function () {
+        var t = $(this).text()
+        var spl = t.split(/:\s*/);
+        var topic = $.trim(spl[0])
+        console.log(topic)
+        if (spl.length > 1 && topic.match(/^(Producers|Genre|Writers|Stars|Genres|Directors)$/)) {
+            var words = spl[1].split(/\s*,\s*/)
+            return topic + ": " + $.map(words, function (e) {
+                e = e.replace(/L$/,"")
+                var q = e.replace(/\(.*?\)/g,"")
+				var q = q.replace(/\./g,"")
+                var l = '<a href="/search/?q=' + q + '">' + e + '</a>';
+                return l;
+            }).join(", ")
+        }else{
+            return t
+        }
+    });
+})
