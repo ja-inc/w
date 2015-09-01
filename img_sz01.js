f03e41769f4b4804fa98d1556f41229ebd0f5983
Jheb Krueger <jheb93@gmail.com>
2015-09-02 03:12:22 +0700

    Create img_sz01.js

---
+++ img_sz01.js
@@ -0,0 +1,16 @@
+function resizeThumb(el, from, to) {
+    $(el).each(function () {
+        $(this).attr({
+            'src': $(this).attr('src').replace('/s' + from + '-c/', '/w' + to),
+            'width': to,
+            'height': to
+        });
+    });
+}
+$(function () {
+  resizeThumb('#PopularPosts1 img', '72', '80');
+  resizeThumb('div .post-body img', '1600', '225');
+	resizeThumb('div .post-body img', '320', '225');
+	resizeThumb('div #related-posts img', '1600', '175');
+	resizeThumb('div #related-posts img', '320', '175');
+});
