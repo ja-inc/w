311730f8fe78050217f8d61a256426bff1edd3ee
Jheb Krueger <jheb93@gmail.com>
2015-09-02 03:39:10 +0700

    Update main_01.js

--- main_01.js
+++ main_01.js
@@ -6,3 +6,13 @@ $(document).ready(function () {
         indent: '-'
     });
 });
+//menu page highlight
+$(function () {
+    var url = window.location.href;
+    $('#menu-wrapper a').each(function () {
+        if (url == (this.href)) {
+            $(this).closest('ul').parent('li').addClass('active');
+            $(this).closest("li").addClass('active');
+        }
+    });
+});
