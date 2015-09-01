63dd6bd7d66dc5fe892bdc78b6288255d24d2c98
Jheb Krueger <jheb93@gmail.com>
2015-09-02 06:07:47 +0700

    Update m_main01.js

--- m_main01.js
+++ m_main01.js
@@ -48,3 +48,14 @@ $(document).ready(function () {
         })
     })
 });
+function resizeThumb(i, e, s) {
+    $(i).each(function () {
+        $(this).attr({
+            src: $(this).attr("src").replace("/s" + e, "/w" + s),
+            width: s
+        })
+    })
+}
+$(function () {
+    resizeThumb("div .post-body img", "1600", "225"), resizeThumb("div .post-body img", "320", "225"), resizeThumb("div #related-posts img", "1600", "175"), resizeThumb("div #related-posts img", "320", "175")
+});
