252150d97b18b214c9efee1b24436db3909fabc4
Jheb Krueger <jheb93@gmail.com>
2015-09-02 20:42:11 +0700

    Create t_ftslider.js

---
+++ t_ftslider.js
@@ -0,0 +1,1 @@
+featuredcontentslider.init({id:"slider1",contentsource:["inline",""],toc:"#increment",nextprev:["Previous","Next"],enablefade:[true,0.5],autorotate:[true,6000],onChange:function(b,a){}});
