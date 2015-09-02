fffcd30495965359c3b45dee9fe9ba017c91ac93
Jheb Krueger <jheb93@gmail.com>
2015-09-03 05:55:02 +0700

    Create m_sfcbtwapi.js

---
+++ m_sfcbtwapi.js
@@ -0,0 +1,13 @@
+(function(i,g,h){var j,d=i.getElementsByTagName(g)[0];
+if(i.getElementById(h)){return}j=i.createElement(g);
+j.id=h;j.src="http://connect.facebook.net/en_US/all.js#xfbml=1";
+d.parentNode.insertBefore(j,d)}(document,"script","facebook-jssdk"));
+(function(){var d=document.createElement("script");
+d.type="text/javascript";d.async=true;
+d.src="https://apis.google.com/js/plusone.js";
+var c=document.getElementsByTagName("script")[0];
+c.parentNode.insertBefore(d,c)})();
+!function(i,g,h){var j,d=i.getElementsByTagName(g)[0];
+if(!i.getElementById(h)){j=i.createElement(g);
+j.id=h;j.src="//platform.twitter.com/widgets.js";
+d.parentNode.insertBefore(j,d)}}(document,"script","twitter-wjs");
